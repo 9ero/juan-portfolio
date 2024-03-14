cecb6c486bae341f93557c6fd0e6be9f33083a2e
Juan Miguel Fernandez <82823629+9ero@users.noreply.github.com>
2024-03-14 17:58:31 -0600

    Fixing about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,4 @@
+
 export default function aboutPage(){
     return <div className="min-h-screen">
         <div className=" min-h-screen w-full flex flex-col justify-center items-center">
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,11 @@
 import Contact from "@/components/Contact";
+export const metadata = {
+    title: "Contact - Juan Miguel's Portfolio",
+    description: 'Full stack developer portfolio',
+  }
 
-export default function(){
+export default function ContactPage(){
+    
     return <div className="min-h-screen w-screen">
      <div className="min-h-screen w-full items-center  flex justify-center">
         <Contact/>
